15c746e3907dfe6399dcae6a1412baaa80e3adb4
Michael Brooks <michael@navkor.com>
2022-12-18 15:55:10 -0700

    finished videos on Dec 17 2022

---
+++ 1-node-farm/starter/index.js
@@ -0,0 +1,51 @@
+const fs = require('fs');
+const http = require('http');
+/////////////////////////////////////
+//
+//
+//   FILES
+//
+//
+//
+/* //Blocking, synchronous way
+const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
+console.log(textIn);
+
+const textOut = `This is what we know about the avocado: ${textIn}.\nCreated on ${Date.now()}`;
+fs.writeFileSync('./txt/output.txt', textOut);
+
+console.log('File written!'); */
+
+
+//non-blocking, asynchronous way
+/*
+    this is going to be a bit crazy!!
+*/
+/*
+fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
+    if (err) return console.log('Error!!! ');
+    fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
+        console.log(data2);
+        fs.readFile('./txt/append.txt', 'utf-8', (err, data3) => {
+            console.log(data3);
+
+            fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, 'utf-8', err => {
+                console.log('Your file has been written yay!!!');
+            });
+        });
+    });
+});
+console.log('will read file!');
+*/
+
+///////////////////////////////////
+// SERVER
+
+const server = http.createServer((req, res) => {
+    //console.log(req);
+    res.end('Hello from the server!!');
+});
+
+server.listen(8001, '127.0.0.1', () => {
+    console.log('listening to requests on port 8001');
+})
