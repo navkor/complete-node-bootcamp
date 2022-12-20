6cbd0d50469fafa8117e4d64886ce6f1efee13e8
Michael Brooks <michael@navkor.com>
2022-12-19 19:08:48 -0700

    finished setting up the cards

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -43,14 +43,29 @@ console.log('will read file!');
 
 ///////////////////////////////////
 // SERVER
+const replaceTemplate = (temp, product) => {
+    let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
+    output = output.replace(/{%IMAGE%}/g, product.image);
+    output = output.replace(/{%PRICE%}/g, product.price);
+    output = output.replace(/{%FROM%}/g, product.from);
+    output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+    output = output.replace(/{%QUANTITY%}/g, product.quantity);
+    output = output.replace(/{%DESCRIPTION%}/g, product.description);
+    output = output.replace(/{%ID%}/g, product.id);
+
+    if (!product.organic){
+        output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
+    }
+    return output;
+}
 
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
 console.log('this only runs once');
 const dataObj = JSON.parse(data);
 
 const templateOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8');
-const templateProduct = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8');
-const templateCard = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8');
+const templateCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8');
+const templateProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8');
 
 const server = http.createServer((req, res) => {
     //console.log(req);
@@ -65,7 +80,9 @@ const server = http.createServer((req, res) => {
             'Content-type': 'text/html'
         });
 
-        res.end(templateOverview);
+        const cardsHTML = dataObj.map(el => replaceTemplate(templateCard, el)).join('');
+        const output = templateOverview.replace('{%PRODUCT_CARDS%}', cardsHTML);
+        res.end(output);
 
     
     //product page
