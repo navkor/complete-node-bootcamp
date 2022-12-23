76f58aea6e1ac0185fe8e5ac25d67d967b3e170a
Michael Brooks <michael@navkor.com>
2022-12-22 19:17:59 -0700

    improved server app and environment variables

--- 4-natours/starter/server.js
+++ 4-natours/starter/server.js
@@ -1,4 +1,9 @@
+const dotenv = require('dotenv');
+dotenv.config({ path: './config.env' });
 const app = require('./app');
+
+
+
 const port = process.env.PORT || 8001;
 //server
 app.listen(port, () => {
